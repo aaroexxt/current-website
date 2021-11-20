824b0f0403a003ba66153e51d728cc1268880e4a
aaroexxt <aaron.becker@gmail.com>
2021-11-19 22:02:49 -0500

    le earliest updates

---
+++ src/home.jsx
@@ -0,0 +1,15 @@
+import './home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <header className="App-header">
+        <p>
+          Testing
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
