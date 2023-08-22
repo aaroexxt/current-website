db3a73f3bd23df4f82da5bbaa1548e23d90ee747
Aaron Becker <aaron.becker@gmail.com>
2023-08-22 00:00:38 -0500

    working offline pdfjs-dist, need to upgreade react

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,9 @@
-const path = require('path');
+// const path = require('path');
 
-module.exports = {
-    webpack: function (config, env) {    
-        config.resolve.alias['pdfjs-dist'] = path.join(__dirname, './node_modules/react-pdf/node_modules/pdfjs-dist/build/pdf.min.js');
-        return config;
-    }
-};
+// module.exports = {
+//     webpack: function (config, env) {    
+//         config.resolve.alias['pdfjs-dist'] = path.join(__dirname, './node_modules/pdfjs-dist');
+//         return config;
+//     }
+// };
 
